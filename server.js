333cf69caa93a8c74aa376504a0279281291af4c
pinglu85 <luping8576@hotmail.com>
2020-04-13 06:31:37 +0200

    add sever side  functions that enable video chat

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const {
+  userJoin,
+  getJoinedUsers,
+  getCurrentUser,
+  userLeave,
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,10 +16,46 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Runs when client connected
+// Runs when client connects
 io.on('connection', (socket) => {
-  socket.on('join', ({ username }) => {
-    console.log(username);
+  socket.on('userJoin', ({ username }) => {
+    const user = userJoin(socket.id, username);
+    const joinedUsers = getJoinedUsers(socket.id);
+
+    socket.broadcast.emit('newUser', user);
+    socket.emit('joinedUsers', joinedUsers);
+  });
+
+  socket.on('callUser', ({ offer, to }) => {
+    const currentUser = getCurrentUser(socket.id);
+    console.log('offer: ', offer);
+    console.log('to: ', to);
+    socket.to(to).emit('callMade', {
+      offer: offer,
+      username: currentUser.username,
+      id: currentUser.id,
+    });
+  });
+
+  socket.on('makeAnswer', ({ answer, to }) => {
+    console.log('answer: ', answer);
+    socket.to(to).emit('answerMade', {
+      socket: socket.id,
+      answer: answer,
+    });
+  });
+
+  socket.on('rejectCall', ({ from }) => {
+    const currentUser = getCurrentUser(socket.id);
+    socket.to(from).emit('callRejected', currentUser.username);
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.emit('userLeave', user.id);
+    }
   });
 });
 
